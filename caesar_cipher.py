39a38bb76940079debf9f27e75b98e8ca16d7fc8
mahdiakil <mahdiakil@live.com>
2023-01-08 00:11:42 +0100

    small changes

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,33 +1,45 @@
 #!/usr/bin/python3
 
 def main():
-    LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-
     message = input("Enter a message: ").upper()
     key = int(input('Enter your key:' ))
+    # Make sure key is not greater than the number of letters
     if key > 25:
         key = key % 26
-
     mode = mode_fn()
-    translated = ''
+    if mode == 1:
+        print(encrypt_caesar(message, key))
+    elif mode == 2:
+        print(decrypt_caesar(message, key))
 
+def encrypt_caesar(message, key):
+    translated = ''
     for m in message:
         if m in LETTERS:
             num = LETTERS.find(m)
-            if mode == 1:
-                num = (num + key)
-                if num > 25:
-                    num = num % 26
-                translated += LETTERS[num]
-            elif mode == 2:
-                num = num - key
-                if num < 0:
-                    num = num + len(LETTERS)
-                translated += LETTERS[num]
+            num = (num + key)
+            if num > 25:
+                num = num % 26
+            translated += LETTERS[num]
         else:
+            # If there is a special character, just add it..
             translated += m
 
-    print(f"translated: {translated}")
+    return translated
+
+def decrypt_caesar(message, key):
+    translated = ''
+    for m in message:
+        if m in LETTERS:
+            num = LETTERS.find(m)
+            num = num - key
+            if num < 0:
+                num = num +len(LETTERS)
+            translated += LETTERS[num]
+        else:
+            # If there is a special character, just add it..
+            translated += m
+    return translated
 
 def mode_fn():
     ans = int(input("choose '1' to encrypt and '2' to decrypt '0' to exit: "))
@@ -40,6 +52,7 @@ def mode_fn():
 
     
 if __name__ == '__main__':
+    LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
     main()
 
 
