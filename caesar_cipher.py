7ad3f9e986079ae5ed4831097fe5f59d52d2fb72
mahdiakil <mahdiakil@live.com>
2023-01-07 22:35:37 +0100

    Caesar cipher encrypt/decrypt and hack

---
+++ caesar_cipher.py
@@ -0,0 +1,42 @@
+#!bin/python3
+
+def mode_fn():
+    ans = int(input("choose '1' to encrypt and '2' to decrypt '0' to exit: "))
+    while ans not in [0, 1, 2]:
+        ans = int(input('choose 1 to encrypt and 2 to decrypt: '))
+    if ans == 0:
+        exit()
+    return ans
+
+
+LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+message = input("Enter a message: ").upper()
+key = int(input('Enter your key:' ))
+if key > 25:
+    key = key % 26
+
+mode = mode_fn()
+translated = ''
+
+for m in message:
+    if m in LETTERS:
+        num = LETTERS.find(m)
+        if mode == 1:
+            num = (num + key)
+            if num > 25:
+                num = num % 26
+            translated += LETTERS[num]
+        elif mode == 2:
+            num = num - key
+            if num < 0:
+                num = num + len(LETTERS)
+            translated += LETTERS[num]
+    else:
+        translated += m
+
+print(f"translated: {translated}")
+    
+
+
+
---
+++ hacking_caesar_cipher.py
@@ -0,0 +1,22 @@
+#!bin/python3
+
+LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+message = input("Enter a message: ").upper()
+i = 0
+translate = ''
+while i < 26:
+    for m in message:
+        if m in LETTERS:
+            loc = LETTERS.find(m)
+            new_loc = loc - i
+            if new_loc < 0:
+                new_loc = new_loc + len(LETTERS)
+            translate += LETTERS[new_loc]
+        else:
+            translate += m
+    print(f"Key {i}: {translate}")
+    translate = ''
+    i += 1        
+
+
